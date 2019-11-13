9d44ed629d3808bf4f6536f52e18f317f7fe7c44
jp <joao@ubots.com.br>
2019-11-12 21:10:29 -0300

    Adicionando logica de loop do game

--- model/jogador.js
+++ model/jogador.js
@@ -1,11 +1,30 @@
-class Jogador{
+class Jogador {
     constructor(valor, cartas) {
         this.valor = valor
         this.cartas = cartas
         this.playable = true
     }
 
-    play(mesa) {
-        
+    play(mesa, quit) {
+        if(quit) {
+            this.playable = false
+        } else {
+            this.mesa.tablePit += this.getBet()
+        }
+    }
+
+    receiveCards(cards) {
+        this.cartas = cards
+    }
+
+    getBet() {
+        if (this.valor < 50) {
+            let value = this.valor
+            this.valor = 0
+            return value
+        } else {
+            this.valor -= 50
+            return 50
+        }
     }
 }
