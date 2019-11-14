f010c782e461f66475af74d53b7447809c89cee3
jp <joao@ubots.com.br>
2019-11-14 12:18:15 -0300

    Importando scritps no body, mantendo o window.onload pq é mais bonito e 'seguro'

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-window.onload = () =>{
-  
-let deck = new Deck();
+window.onload = () => {
+    let deck = new Deck();
 
-deck.shuffle();
+    deck.shuffle();
 
-var card1 = document.getElementById("img1");
-var card2 = document.getElementById("img2");
+    var card1 = document.getElementById("img1");
+    var card2 = document.getElementById("img2");
 
-console.log(card1);
-console.log(card2);
+    console.log(card1);
+    console.log(card2);
 
 
-card1.src = deck.cards[0].frontImage;
-card2.src = deck.cards[1].frontImage;  
+    card1.src = deck.cards[0].frontImage;
+    card2.src = deck.cards[1].frontImage;
 }
